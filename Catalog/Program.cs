cf2336d46b5572ab6e83271d5972ba6589bbf86b
Deniz <denizdogru97@gmail.com>
2025-04-04 00:01:50 +0300

    Added AddNpgsqlDbContext to the Services container to enable communication w/ postgre,
    Added GlobalUsings

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.AddServiceDefaults();
 
+builder.AddNpgsqlDbContext<ProductDbContext>(connectionName: "catalogDb");
+
 var app = builder.Build();
 
 

cf428537dd0d283155040e3737ae18e31e33da68
Deniz <denizdogru97@gmail.com>
2025-04-05 18:35:44 +0300

    Basket endpoint registered to program.cs, global usings updated. Testing how basket microservice uses redis distributed cache in this step

---
+++ Basket/Services/BasketService.cs
@@ -0,0 +1,27 @@
+﻿using Basket.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.Services;
+
+//  Add caching logic.
+public class BasketService(IDistributedCache cache) // Using primary constructor
+{
+    public async Task<ShoppingCart?> GetBasket(string userName)
+    {
+        var basket = await cache.GetStringAsync(userName);
+        if (string.IsNullOrEmpty(basket))
+            return null;
+        return JsonSerializer.Deserialize<ShoppingCart>(basket);
+    }
+
+    public async Task UpdateBasket(ShoppingCart basket)
+    {
+        await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
+    }
+
+    public async Task DeleteBasket(string userName)
+    {
+        await cache.RemoveAsync(userName);
+    }
+}
